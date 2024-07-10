2131cb7efc026e076bffb12d0d6bf3d979e7f814
Jayantapte25 <132655232+Jayantapte25@users.noreply.github.com>
2024-07-10 17:12:35 +0530

    Delete JAVA directory

--- JAVA/SQLITE/CRUD/Query.java
+++
@@ -1,8 +0,0 @@
-package SQLITE.CRUD;
-
-public class Query {
-    static String Create = "INSERT INTO employees (id, last_name, first_name, email, department, salary) VALUES (?, ?, ?, ?, ?, ?)";
-    static String Update = "UPDATE employees SET last_name = ?, first_name = ?, email = ?, department = ?, salary = ? WHERE id = ?";
-    static String Delete = "DELETE FROM employees WHERE id = ?";
-    static String Read = "SELECT * FROM employees";
-}
